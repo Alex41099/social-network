bab5acfaec228ec18b323187051562dcc3e7f436
Alex41099 <nevazhnoalikhan@gmail.com>
2024-11-28 19:56:04 +0500

    commit

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,14 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
 import { store } from "./app/store"
-import { RouterProvider } from "react-router-dom"
+import {HashRouter, RouterProvider} from "react-router-dom"
 import { router } from "./app/routes/router"
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement)
 root.render(
   <Provider store={store}>
-    <RouterProvider router={router} />
+      <HashRouter basename={process.env.PUBLIC_URL}>
+          <RouterProvider router={router} />
+      </HashRouter>
   </Provider>,
 )
